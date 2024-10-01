b8e78934518b64ee15301d1e1e08d02be627bc95
Emil Gajšak <emil.gajsak@gmail.com>
2024-10-01 11:16:38 +0200

    Pins for HW v0.2

--- main/src/Pins.hpp
+++ main/src/Pins.hpp
@@ -1,29 +1,29 @@
 #ifndef CLOCKSTAR_LIBRARY_PINS_HPP
 #define CLOCKSTAR_LIBRARY_PINS_HPP
 
-#define BTN_LEFT 41
-#define BTN_RIGHT 42
-#define BTN_A 13
-#define BTN_B 12
+#define BTN_LEFT 5
+#define BTN_RIGHT 4
+#define BTN_A 3
+#define BTN_B 2
 
 #define BTN_UP BTN_LEFT
 #define BTN_DOWN BTN_RIGHT
 
-#define PIN_BL 39
-#define PIN_BUZZ 40
+#define PIN_BL 43
+#define PIN_BUZZ 44
 #define PIN_BATT 1
 
-#define I2C_SDA 3
-#define I2C_SCL 2
+#define I2C_SDA 33
+#define I2C_SCL 34
 
-#define TFT_SCK 35
-#define TFT_MOSI 36
-#define TFT_DC 37
-#define TFT_RST 38
+#define TFT_SCK 39
+#define TFT_MOSI 40
+#define TFT_DC 41
+#define TFT_RST 42
 
-#define LED_G 43
-#define LED_Y 44
-#define LED_O 15
-#define LED_R 14
+#define LED_G 35
+#define LED_Y 36
+#define LED_O 37
+#define LED_R 38
 
 #endif //CLOCKSTAR_LIBRARY_PINS_HPP
